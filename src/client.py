54fd2d2337a35883ae4974cea3f7b16ab403b30b
Amiel Berchenko <amielberchenko@lawn-128-61-76-179.lawn.gatech.edu>
2019-11-12 14:21:10 -0500

    Main project completed

---
+++ src/client.py
@@ -0,0 +1,138 @@
+# client.py
+
+from socket import *
+
+import sys
+
+debugging = False
+maxIncorrect = 6
+
+ip = '127.0.0.1'
+port = '9012'
+
+def debug(*messages):
+    if debugging:
+        for message in messages:
+            print(message, end=" ")
+        print("")
+
+class Connection:
+    def __init__(self, socket):
+        self.socket = socket
+        self.guesses = []
+        self.wrongGuesses = []
+
+        self.sendMessage("")
+        self.gameLoop()
+
+    def gameLoop(self):
+        while True:
+            gameInfo = self.receiveMessage()
+            if gameInfo[0] == 0:
+                self.printWord(gameInfo[3])
+                self.processResult(gameInfo)
+                self.printWrongGuesses()
+            else:
+                print(gameInfo[1])
+                self.endConnection()
+                break
+
+            if len(self.wrongGuesses) < maxIncorrect and "-" in gameInfo[3]:
+                letter = self.getGuess()
+                self.guesses.append(letter)
+                self.sendMessage(letter)
+
+    def getGuess(self):
+        letter = ''
+        foundLetter = False
+        while not foundLetter:
+            print("Letter to guess:", end=" ")
+            letter = input()
+            verifiedLetter = self.verifyLetter(letter)
+            if verifiedLetter == '':
+                print("Error! Please guess one letter.")
+            else:
+                if verifiedLetter in self.guesses:
+                    print("Error! Letter", verifiedLetter, "has been guessed before, please guess another letter.")
+                else:
+                    foundLetter = True
+
+        return letter
+
+    def printWord(self, word):
+        for letter in word:
+            print(letter, end=" ")
+        print("")
+
+    def printWrongGuesses(self):
+        print("Incorrect Guesses:", end="")
+        for letter in self.wrongGuesses:
+            print(" " + letter, end="")
+        print("\n")
+
+    def verifyLetter(self, letter):
+        if len(letter) != 1:
+            return ''
+        lower = letter.lower()
+        if lower >= 'a' and lower <= 'z':
+            return lower
+        else:
+            return ''
+
+    def processResult(self, gameInfo):
+        if gameInfo[2] > len(self.wrongGuesses):
+            self.wrongGuesses.append(self.guesses[-1])
+
+    def sendMessage(self, message):
+        messageBytes = message.encode()
+        lengthBytes = (len(messageBytes)).to_bytes(1, 'big')
+
+        finalMessage = lengthBytes + messageBytes
+        self.socket.send(finalMessage)
+
+    def receiveMessage(self):
+        message = self.socket.recv(1024)
+        debug("Received message:", message)
+        return self.processIncomingMessage(message)
+
+    def processIncomingMessage(self, rawMessage):
+        flag = rawMessage[0]
+        if flag != 0:
+            message = rawMessage[1:1+flag].decode()
+            return [flag, message]
+        else:
+            wordLength = rawMessage[1]
+            numIncorrect = rawMessage[2]
+            word = rawMessage[3:3+wordLength].decode()
+            return [flag, wordLength, numIncorrect, word]
+
+    def endConnection(self):
+        self.socket.close()
+
+def startGame():
+    print("Ready to start game? (y/n):", end=" ")
+    answer = input()
+    if answer == 'y':
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((ip, int(port)))
+        Connection(clientSocket)
+
+if __name__ == '__main__':
+
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == '-D':
+            debugging = True
+            continue
+        
+        if not debugging:
+            if i == 1:
+                ip = sys.argv[i]
+            elif i == 2:
+                port = sys.argv[i]
+        else:
+            if i == 2:
+                ip = sys.argv[i]
+            elif i == 3:
+                port = sys.argv[i]
+    
+    startGame()
---
+++ src/server.py
@@ -0,0 +1,207 @@
+# server.py
+
+from socket import *
+
+import sys
+import random
+import threading
+import time
+
+debugging = False
+words = []
+maxIncorrect = 6
+
+ip = '127.0.0.1'
+port = '9012'
+
+maxConnections = 3
+connections = []
+
+def debug(*messages):
+    if debugging:
+        for message in messages:
+            print(message, end=" ")
+        print("")
+
+def readWords():
+    try:
+        wordFile = open("../word.txt", "r")
+
+        try:
+            for line in wordFile:
+                line = line.strip()
+                words.append(line)
+
+        finally:
+            wordFile.close()
+    except:
+        print("Unable to find word.txt")
+
+def initializeConnections():
+    for _ in range(maxConnections):
+        connections.append(None)
+
+def addConnection(newConnection):
+    for i in range(maxConnections):
+        if connections[i] == None:
+            connections[i] = newConnection
+            return i
+    return -1
+
+class Game:
+    def __init__(self):
+        wordIndex = random.randint(0, len(words)-1)
+        self.word = words[wordIndex]
+        self.guesses = []
+
+        print(self.word)
+
+    def guess(self, letter):
+        self.guesses.append(letter)
+
+        if self.numWrongGuesses() >= maxIncorrect:
+            return "You Lose!"
+        elif self.finishedWord():
+            return "You Win!"
+    
+    def numWrongGuesses(self):
+        wrongGuesses = 0
+        for guess in self.guesses:
+            if guess not in self.word:
+                wrongGuesses += 1
+        return wrongGuesses
+
+    def finishedWord(self):
+        for letter in self.word:
+            if letter not in self.guesses:
+                return False
+        return True
+        
+    def getWordSoFar(self):
+        wordSoFar = ""
+        for letter in self.word:
+            if letter in self.guesses:
+                wordSoFar += letter
+            else:
+                wordSoFar += "-"
+        return wordSoFar
+
+class Connection:
+    def __init__(self, socket, addr):
+        self.socket = socket
+        self.addr = addr
+        self.game = Game()
+
+    def startGame(self, index):
+        self.index = index
+        thread = threading.Thread(target=self.startGameAsync, args=[])
+        thread.start()
+
+    def startGameAsync(self):
+        debug("Game starting")
+        message = self.socket.recv(1024)
+        debug("Received initial message:", message)
+        self.sendGuessResult()
+        self.connection_loop()
+
+    def connection_loop(self):
+        while True:
+            message = self.socket.recv(1024)
+            debug("Received message:", message)
+            letter = self.decodeMessage(message)
+            if self.receiveGuess(letter):
+                break
+
+    def receiveGuess(self, guess):
+        result = self.game.guess(guess)
+        debug("Result of guess:", result)
+        self.sendGuessResult()
+        if result != None:
+            time.sleep(0.1)
+            self.sendMessage(result)
+            self.endConnection()
+            return True
+        else:
+            return False
+
+    def sendGuessResult(self):
+        numWrongGuesses = self.game.numWrongGuesses()
+        wordSoFar = self.game.getWordSoFar()
+        wordLength = len(wordSoFar)
+
+        flagBytes = (0).to_bytes(1, 'big')
+        lengthBytes = wordLength.to_bytes(1, 'big')
+        wrongBytes = numWrongGuesses.to_bytes(1, 'big')
+        wordBytes = wordSoFar.encode()
+
+        message = flagBytes + lengthBytes + wrongBytes + wordBytes
+        debug("Sending:", message)
+        self.socket.send(message)
+
+    def decodeMessage(self, message):
+        if len(message) == 0:
+            return None
+        else:
+            length = message[0]
+            letter = message[1:1+length].decode()
+            return letter
+
+    def sendMessage(self, message):
+        bytesMessage = message.encode()
+        messageLength = len(bytesMessage)
+        bytesMessage = messageLength.to_bytes(1, 'big') + bytesMessage
+        debug("Sending:", bytesMessage)
+        self.socket.send(bytesMessage)
+
+    def failConnection(self):
+        debug('Server overloaded')
+        thread = threading.Thread(target=self.failConnectionAsync, args=[])
+        thread.start()
+
+    def failConnectionAsync(self):
+        self.sendMessage('server-overloaded')
+        self.endConnection()
+
+    def endConnection(self):
+        self.socket.close()
+        connections[self.index] = None
+        debug("Connection ended")
+
+def server_loop():
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind((ip, int(port)))
+    serverSocket.listen(1)
+    debug('Server is listening')
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        connection = Connection(connectionSocket, addr)
+        index = addConnection(connection)
+        if index == -1:
+            connection.failConnection()
+        else:
+            connection.startGame(index)
+
+if __name__ == '__main__':
+
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == '-D':
+            debugging = True
+            continue
+        
+        if not debugging:
+            if i == 1:
+                ip = sys.argv[i]
+            elif i == 2:
+                port = sys.argv[i]
+        else:
+            if i == 2:
+                ip = sys.argv[i]
+            elif i == 3:
+                port = sys.argv[i]
+    
+    readWords()
+    initializeConnections()
+
+    debug("Started")
+
+    server_loop()
